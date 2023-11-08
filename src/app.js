00a9ff2452f8cfd998f9bb330bcb0412342e7be4
alexander torres <alexander20012@hotmail.com>
2023-11-08 18:39:35 -0400

    modelo de base de datos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
@@ -15,7 +14,6 @@ server.name = "API";
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
-server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
---
+++ src/models/usuarios.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class usuarios extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.personas,{ foreignKey: "id_persona"});
+      this.belongsTo(models.statud,{ foreignKey: "id_statud"});
+      this.hasMany(models.carrito,{ foreignKey: "id_usuario"});
+      this.hasMany(models.factura,{ foreignKey: "id_usuario"});
+      this.hasMany(models.favoritos_productos,{ foreignKey: "id_usuario"});
+      this.hasMany(models.review,{ foreignKey: "id_usuario"});
+    }
+  }
+  usuarios.init({
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true,
+    },
+    usuario: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'usuarios',
+  });
+  return usuarios;
+};
