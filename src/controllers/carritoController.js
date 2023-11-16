64ec5f905bd2c2aa528878c3b6b8d305be1d1cfe
Alexander Pedro Jose Torres Apontes <alexander20012@hotmail.com>
2023-11-15 21:05:33 -0400

    cambios

--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -62,6 +62,7 @@ exports.create = async (data)=>{
             let newCarrito= await carrito.create({
                 id_usuario:data.id,
                 id_statud:1,
+                total:0,
                 fecha: new Date().toLocaleDateString().toString()
             });
             if(!newCarrito){
