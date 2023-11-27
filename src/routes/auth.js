45384db784f22a49a1e81fa3f9c57b42bd868bb0
alexander torres <alexander20012@hotmail.com>
2023-11-27 12:05:42 -0400

    cambios

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -14,8 +14,8 @@ router.get('/callback', passport.authenticate('google', { failureRedirect: 'http
         const token = jwt.sign({ user }, env.SECRECT_TOKEN, {
             expiresIn: "1h",
         });
-        res.cookie('token', token);
-        res.cookie('user', JSON.stringify(user));
+        res.cookie('token', token,{SameSite:'none',secure: true});
+        res.cookie('user', JSON.stringify(user),{SameSite:'none',secure: true});
         res.redirect('https://trendy-web-lemon.vercel.app/')
     }
 );
