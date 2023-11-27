c93f32e00ef3ec166f2360034e067e0c32cd8b8f
Maximiliano Marquez <maxy.marquez@hotmail.com>
2023-11-26 23:59:55 -0300

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -15,8 +15,8 @@ router.get('/callback', passport.authenticate('google', { failureRedirect: 'http
         const token = jwt.sign({ user }, env.SECRECT_TOKEN, {
             expiresIn: "1h",
         });
-        res.cookie('token', token, {httpOnly: true, expires: new Date(Date.now() + 3600000), sameSite: 'None', secure: true });
-        res.cookie('user', JSON.stringify(user), {httpOnly: true, expires: new Date(Date.now() + 3600000), sameSite: 'None', secure: true });
+        res.cookie('token', token, {httpOnly: true, expires: new Date(Date.now() + 3600000), SameSite: 'None', secure: true });
+        res.cookie('user', JSON.stringify(user), {httpOnly: true, expires: new Date(Date.now() + 3600000), SameSite: 'None', secure: true });
         res.redirect('https://trendy-web-lemon.vercel.app');
     }
 );
