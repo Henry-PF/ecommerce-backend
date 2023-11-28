f8f3884d3e32ddb337d4184fdc675fc824d16c7f
Alexander Pedro Jose Torres Apontes <alexander20012@hotmail.com>
2023-11-28 00:02:27 -0400

    cambios

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -14,8 +14,8 @@ router.get('/callback', passport.authenticate('google', { failureRedirect: 'http
         const token = jwt.sign({ user }, env.SECRECT_TOKEN, {
             expiresIn: "1h",
         });
-        res.cookie('token', token);
-        res.cookie('user', JSON.stringify(user));
+        res.cookie('token', token,{ sameSite: 'none', secure: true });
+        res.cookie('user', JSON.stringify(user),{ sameSite: 'none', secure: true });
         res.redirect('https://trendy-web-lemon.vercel.app/')
     }
 );
