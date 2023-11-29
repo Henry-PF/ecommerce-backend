afc187e3b4715cbd0a06a4e8f47e692310f3d6f8
Maxy Marquez <maxy.marquez@hotmail.com>
2023-11-29 13:51:31 -0300

    cambios

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const {CreateProducto,getAllProducto,getOneProducto,DeleteProducto,UpdateProducto} = require("../handlers/productosHandler");
+const { CreateProducto, getAllProducto, getOneProducto, DeleteProducto, UpdateProducto } = require("../handlers/productosHandler");
 
 router.get("/", getAllProducto);
 router.get("/:id", getOneProducto);
