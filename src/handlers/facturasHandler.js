f812ac6712d9e2cbdfbe6e5ae6991b83f7e25733
Maxy Marquez <maxy.marquez@hotmail.com>
2023-11-29 02:48:26 -0300

    cambios

--- src/handlers/facturasHandler.js
+++ src/handlers/facturasHandler.js
@@ -1,27 +1,40 @@
-const {getFacturas,getFacturaById} = require("../controllers/facturasController");
+const { getFacturas, getFacturaById, getFacturasUsuario } = require("../controllers/facturasController");
 const { logger } = require("../components/logger");
 
-exports.getAllFacturas =async (req, res) => {
+exports.getAllFacturas = async (req, res) => {
     let result = {};
     try {
         result = await getFacturas();
         res.status(200).json(result);
     } catch (error) {
         logger.error(error.message);
-        res.status(500).json({message:error.message,error:true})
+        res.status(500).json({ message: error.message, error: true })
     }
 }
-exports.getOneFacturas =async (req, res) => {
+
+exports.getAllFacturasUsuario = async (req, res) => {
+    let result = {};
+    console.log(req.params);
+    try {
+        result = await getFacturasUsuario(req.params);
+        res.status(200).json(result);
+    } catch (error) {
+        logger.error(error.message);
+        res.status(500).json({ message: error.message, error: true })
+    }
+}
+
+exports.getOneFacturas = async (req, res) => {
     let result = {};
     try {
-        if(req.params){
+        if (req.params) {
             result = await getFacturaById(req.params)
-        }else{
-            result = {message:"faltan campos",error:true}
+        } else {
+            result = { message: "faltan campos", error: true }
         }
         res.status(200).json(result);
     } catch (error) {
         logger.error(error.message);
-        res.status(500).json({message:error.message,error:true})
+        res.status(500).json({ message: error.message, error: true })
     }
 }
